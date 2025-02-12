73ae433b74d04d87b209e49ece0f17dcddf824b5
simplicityf <omobolanlehazeezat@gmail.com>
2025-02-12 09:28:52 +0100

    Testing server reload

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ async def health_check():
 @app.get("/devcheck")
 async def health_check():
     """Checks if server is active."""
-    return {"status": "dev is active"}
+    return {"status": "dev is very active, server changes"}
